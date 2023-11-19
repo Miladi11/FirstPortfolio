620d012d02588b6ed5e7f6e5cf91eeb5f0bd6809
Milad <mtz.milad@gmail.com>
2023-11-19 14:11:14 +0100

    Socialmedia icons

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
+
+        // Select all elements with the 'shake-on-hover' class
+        const shakeOnHoverElements = document.querySelectorAll('.shake-on-hover');
+    
+        // Add a mouseover event listener to each element
+        shakeOnHoverElements.forEach((element) => {
+            element.addEventListener('mouseover', () => {
+                // Add the Animate.css classes to trigger the 'shake' animation
+                element.classList.add('animate__animated', 'animate__shakeX');
+            });
+    
+            // Remove the Animate.css classes when the mouse leaves
+            element.addEventListener('mouseout', () => {
+                element.classList.remove('animate__animated', 'animate__shakeX');
+            });
+        });
